ba6a66185b2c0a15846fcdb8a8eb78e67604e213
Salvador Jimenez Gastelum <salvador.jimenez@agilethought.com>
2020-05-04 02:23:47 -0500

    Added Model and Context

---
+++ Models/Food.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dockertraining_compose_salvador_jimenez.Models
+{
+    public class Food
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+    }
+}
---
+++ Models/FoodContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dockertraining_compose_salvador_jimenez.Models
+{
+    public class FoodContext : DbContext
+    {
+        public DbSet<Food> Foods { get; set; }
+
+        public FoodContext(DbContextOptions options) : base(options) { }
+    }
+}
