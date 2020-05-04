ed08a7a5f05e1d27f9a1cdba28b0670bc89c0038
Salvador Jimenez Gastelum <salvador.jimenez@agilethought.com>
2020-05-04 02:55:56 -0500

    Migration success

---
+++ Models/DesignTimeDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dockertraining_compose_salvador_jimenez.Models
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<FoodContext>
+    {
+        public FoodContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<FoodContext>();
+            var connectionString = configuration.GetSection("ConnectionStrings").GetSection("FoodDB").Value;
+            builder.UseSqlServer(connectionString);
+            return new FoodContext(builder.Options);
+        }
+    }
+}
